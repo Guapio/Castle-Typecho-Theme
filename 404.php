5af7ca21299292ca6d33b85020323d52d9f53bb0
ohmyga <40896676+ohmyga233@users.noreply.github.com>
2018-09-09 17:44:29 +0800

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,12 +1,13 @@
 <?php
  if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  $this->need('header.php');
+ $style = Typecho_Widget::widget('Widget_Options')->style;
  ?>
  <!-- 内容区 -->
  <div class="mdui-container" id="moe-main">
  <div style="margin-top:180px;"></div>
  <div class="mdui-text-center">
- <font size="20" class="<?php if ($style == "bg_style"){ ?>mdui-text-color-<?php echo Typecho_Widget::widget('Widget_Options')->icon_color; ?><?php }elseif ($style == "default_style"){}?>">404 Not Found</font>
+ <div class="<?php if ($style == "bg_style"){ ?>mdui-text-color-<?php echo Typecho_Widget::widget('Widget_Options')->icon_color; ?><?php }elseif ($style == "default_style"){}?>" style="font-size: 50px;">404 Not Found</div>
  <br>
  <img src="<?php $this->options->themeUrl('others/images/404.gif'); ?>">
  </div>
